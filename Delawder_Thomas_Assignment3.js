582aa72a03cca657d5a41badc60ce63191f9e6bf
b07080 <tom@turbotektuning.com>
2013-06-20 11:18:33 -0300

    building an object for storage unit A

--- Delawder_Thomas_Assignment3.js
+++ Delawder_Thomas_Assignment3.js
@@ -1,1 +1,31 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Tom Delawder
+// SDI 1306
+// Project 3
+
+
+
+
+// Object - Storage Unit A
+
+var storageUnit = {
+    
+    "name": "Unit A",
+    "height": 10,
+    "length": 10,
+    "width": 5,
+    "area": function(){
+        
+        var totalArea = this.height * this.length * this.width;
+        
+        return totalArea;        
+        
+    } // End Area Method
+    
+    
+} // End storageUnit object
+  
+var theArea = storageUnit.area();
+
+console.log("The area of the unit is " + theArea);
