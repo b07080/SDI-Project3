183965bb38f8c99ede47c227d5ebe95b5efc11e8
b07080 <tom@turbotektuning.com>
2013-06-20 11:39:03 -0300

    added method accessor and mutator

--- Delawder_Thomas_Assignment3.js
+++ Delawder_Thomas_Assignment3.js
@@ -15,17 +15,25 @@ var storageUnit = {
     "height": 10,
     "length": 10,
     "width": 5,
-    "area": function(){
+    "area": function(){      // Method Accessor
         
         var totalArea = this.height * this.length * this.width;
         
         return totalArea;        
         
-    } // End Area Method
-    
+    }, // End Area Method
+    "setLength": function(newLength) { // Method Mutator
+        
+        this.length = newLength;
+        
+    } // End Set Length
     
 } // End storageUnit object
   
 var theArea = storageUnit.area();
 
 console.log("The area of the unit is " + theArea);
+
+storageUnit.setLength(20);
+
+console.log("The new area is " + storageUnit.area());
